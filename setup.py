4a8b9b801ade8b3184ef22afa2ef901e2a81f4e8
Asish Kumar <officialasishkumar@gmail.com>
2024-10-15 11:23:32 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='Sickle_Cell',
+    version='1.0',
+    packages=[''],
+    package_dir={'': 'src'},
+    url='https://github.com/AsishKumar/Sickle-Cell-Anemia-Detection',
+    author='Asish Kumar',
+    author_email='officialasishkumar@gmail.com',
+    description='Computer Vision Sickle Cell Anemia Detection',
+    install_requires = [
+        "matplotlib",
+        "opencv-python",
+        "numpy",
+        "scikit-image",
+        "pillow",
+        "scipy"
+    ]
+)
